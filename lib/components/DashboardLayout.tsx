4213cbb428893a24b5bacdeab096a668c8c9c4db
mir4na <mirananightfall228@gmail.com>
2026-01-26 18:44:25 +0700

    more integrate

--- lib/components/DashboardLayout.tsx
+++ lib/components/DashboardLayout.tsx
@@ -148,15 +148,6 @@ const getMitraNavItems = (t: (key: string) => string): NavItem[] => [
       </svg>
     ),
   },
-  {
-    href: '/eksportir/profile',
-    label: t('nav.profile'),
-    icon: (
-      <svg className="w-5 h-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={1.5} d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" />
-      </svg>
-    ),
-  },
 ];
 
 interface DashboardLayoutProps {
